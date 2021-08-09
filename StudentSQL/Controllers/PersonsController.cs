4accfcd88e613bfe69ca3299acafc1082db94695
Nabeel Khan <178791@ust-global.com>
2021-08-09 12:35:42 +0100

    logger implementd

--- StudentSQL/Controllers/PersonsController.cs
+++ StudentSQL/Controllers/PersonsController.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using studentdatasql.Respositories;
 using studentdatasql.Models;
 using System.Collections.Generic;
+using Serilog;
 
 namespace StudentSQL.Controllers
 {
@@ -11,40 +12,49 @@ namespace StudentSQL.Controllers
 
     public class PersonsController : Controller
     {
-
         private IConfiguration Configuration;
         private static PersonRepository persons;
+        ILogger _logger;
 
 
-        public PersonsController(IConfiguration _configuration)
+        public PersonsController(IConfiguration _configuration, ILogger logger)
         {
             Configuration = _configuration;
             persons = new PersonRepository(_configuration);
-
+            _logger = logger;
         }
 
         [HttpGet]
         public List<string> GetPersons()
         {
-            return persons.GetPersons();
+            List<string> personList = persons.GetPersons();
+            foreach (string y in personList)
+            {
+                _logger.Information(y);
+            }
+            return personList;
         }
 
         [HttpPost]
         public string PostPerson(Person person)
         {
+
+            _logger.Information(" Person inserted: " + person.Id + " " + person.FirstName + " " + person.LastName + " " + person.SupervisorId + " " + person.BranchId);
             return persons.PostPerson(person);
         }
 
         [HttpPut]
         public string PutPerson(Person person)
         {
+            _logger.Information(" Person updated: " + person.Id + " " + person.FirstName + " " + person.LastName + " " + person.SupervisorId + " " + person.BranchId);
             return persons.PutPerson(person);
         }
 
-        [HttpDelete ("{Id}")] 
+        [HttpDelete("{Id}")]
         public string DeletePerson(int Id)
         {
-            return persons.DeletePerson (Id);
+            _logger.Information("Person with ID number" + Id + " has been deleted from the records");
+            return persons.DeletePerson(Id);
         }
 
     }
